6c4f8385eee65454aa9e05cb84a726d90b2d5d3e
sotanominomi-pixel <sotanominomi@gmail.com>
2025-10-30 03:48:17 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='nclock-v2';
+const urlsToCache=['./','./index.html','./style.css','./script.js'];
+
+self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))); });
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
